808d0911ad733e73d93fe942658fa5f1191d5749
Claudio Masolo <claudio.masolo@nearform.com>
2025-05-05 15:39:05 +0200

    improved UI and new icon

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 # terraform-visualizer 
 
-Terraform Visualizer is a Visual Studio Code extension that helps you visualize and manage your Terraform plans. This extension provides a user-friendly interface to view and filter changes in your Terraform plans, making it easier to understand and manage your infrastructure changes.
+Terraform Visualizer is a Visual Studio Code extension that helps you visualize and manage your Terraform and OpenTofu plans. This extension provides a user-friendly interface to view and filter changes in your Terraform/OpenTofu plans, making it easier to understand and manage your infrastructure changes.
 
 ## Features
 
-- **Visualize Terraform Plans**: View detailed changes in your Terraform plans with a clear and concise interface.
+- **Visualize Terraform and OpenTofu Plans**: View detailed changes in your Terraform/OpenTofu plans with a clear and concise interface.
 - **Filter Changes**: Easily filter changes by action (create, update, delete).
 - **Search Changes**: Easily search for specific changes with a full text search bar.
 - **Toggle Details**: Expand and collapse detailed views of changes to focus on the information you need.
@@ -18,14 +18,24 @@ Terraform Visualizer is a Visual Studio Code extension that helps you visualize 
 ## Requirements
 
 - **Terraform**: Ensure you have Terraform installed on your system. You can download it from the [official Terraform website](https://www.terraform.io/downloads.html).
+- **OpenTofu**: Ensure you have OpenTofu installed on your system. You can follow [the official documentation](https://opentofu.org/docs/intro/install/)
 
 ## How to use the extension
+
+### Terraform
 
 Run your terrafrom plan with:
 
 ```
 terraform plan -out=plan && terraform show -json plan > tfplan.json
 ```
+
+### OpenTofu
+
+```
+tofu plan -out=plan && tofu show -json plan > tfplan.json
+```
+
 Then open TF-visualizer in vs-code command:
 ![command](https://raw.githubusercontent.com/klaus82/tf-visualizer/refs/heads/main/images/image.png) 
 
@@ -36,6 +46,9 @@ Then open TF-visualizer in vs-code command:
 - **Large Plans**: Performance may be impacted when visualizing very large Terraform plans. We are working on optimizations to improve this.
 
 ## Release Notes
+### 1.2.0
+- Differences and changes UI reviewed.
+  ![differences](https://raw.githubusercontent.com/klaus82/tf-visualizer/refs/heads/main/assets/1.2.0/differences.png)
 
 ### 1.1.0
 - Added search bar for changes
@@ -50,5 +63,5 @@ Then open TF-visualizer in vs-code command:
 
 ## TO DO:
 - [x] Search functionality to find specific changes.
-- [ ] Improve the UI.
+- [x] Improve the UI.
 - [ ] Visualize the `terraform graph -type=plan`
--- src/htmlGenerator.ts
+++ src/htmlGenerator.ts
@@ -78,7 +78,7 @@ export function generateHtmlContent(changes: any[]): string {
             vertical-align: top; /* Align to the top */
         }
         #detailsCell_40 {
-            width: 40%; 
+            width: 5ß0%; 
             word-wrap: break-word;
         }
         .removed { color: #e06c75; } /* Red */
@@ -106,6 +106,31 @@ export function generateHtmlContent(changes: any[]): string {
             color: white;
             padding: 8px; 
             border-radius: 5px;
+        }
+
+        .json-container {
+            flex: 1;
+            margin: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .diff-container {
+            flex: 1;
+            margin: 10px;
+            border: 1px solid #ccc;
+            padding: 10px;
+            overflow-x: auto;
+        }
+        .added {
+            background-color: #ccffcc; /* Light green */
+        }
+
+        .removed {
+            background-color: #ffcccc; /* Light red */
+        }
+
+        .modified {
+            background-color: #ffffcc; /* Light yellow */
         }
 
     </style>
@@ -225,7 +250,9 @@ function generateTableRows(changes: any[]):  { html: string, count: number } {
             color = 'orange';
         }
 
-        let delta = diffString(change.change.before, change.change.after);
+        //let delta = diffString(change.change.before, change.change.after);
+
+        let delta = jsonDiff(change.change.before, change.change.after);
 
         return `
             <tr onclick="toggleDetails(${index})">
@@ -234,16 +261,22 @@ function generateTableRows(changes: any[]):  { html: string, count: number } {
             </tr>
             <tr id="details-${index}" style="display: none;">
                 <td colspan="2">
-                    <table id="detailsTable">
+                    <table width="100%">
                         <tr>
-                            <th>Changes</th>
-                            <th>Before</th>
-                            <th>After</th>
+                        <h3>Diff</h3>
+                        <div class="diff-container">
+                            <pre>${renderDiff(delta)}</pre>
+                        </div>
                         </tr>
                         <tr>
-                            <td class="detailsCell_20"><pre>${renderDiff(delta)}</pre></td>
-                            <td class="detailsCell_40"><pre>${JSON.stringify(change.change.before, null, 2)}</pre></td>
-                            <td class="detailsCell_40"><pre>${JSON.stringify(change.change.after, null, 2)}</pre></td>
+                        <h3>Before</h3>
+                            <div class="json-container">
+                                <pre>${JSON.stringify(change.change.before, null, 2)}</pre>
+                            </div>
+                        <h3>After</h3>
+                            <div class="json-container">
+                                <pre>${JSON.stringify(change.change.after, null, 2)}</pre>
+                            </div>
                         </tr>
                     </table>
                 </td>
@@ -254,9 +287,42 @@ function generateTableRows(changes: any[]):  { html: string, count: number } {
     return { html: rowsHtml, count: changesCount };
 }
 
-function renderDiff(diff:string) {
-    return diff
-        .replace(/\u001b\[31m/g, '<span class="removed">')  // Red start
-        .replace(/\u001b\[32m/g, '<span class="added">')    // Green start
-        .replace(/\u001b\[39m/g, '</span>');                 // End color
+function jsonDiff(obj1:any, obj2:any, path = '') {
+    const diff:any = [];
+    if (obj1 === obj2) {
+      return diff;
+    }
+    const allKeys = new Set([...Object.keys(obj1 || {}), ...Object.keys(obj2 || {})]);
+  
+    allKeys.forEach(key => {
+      const value1 = obj1 ? obj1[key] : undefined;
+      const value2 = obj2 ? obj2[key] : undefined;
+      const currentPath = path ? `${path}.${key}` : key;
+  
+      if (value1 === undefined) {
+        diff.push({ type: 'added', path: currentPath, value: value2 });
+      } else if (value2 === undefined) {
+        diff.push({ type: 'removed', path: currentPath, value: value1 });
+      } else if (typeof value1 === 'object' && typeof value2 === 'object' && value1 !== null && value2 !== null) {
+        diff.push(...jsonDiff(value1, value2, currentPath));
+      } else if (value1 !== value2) {
+        diff.push({ type: 'modified', path: currentPath, oldValue: value1, newValue: value2 });
+      }
+    });
+  
+    return diff;
+  }
+
+function renderDiff(diff:any[]): string {
+    return diff.map(change => {
+        let formattedChange = '';
+        if (change.type === 'added') {
+          formattedChange = `<span class="added">+ ${change.path}: ${JSON.stringify(change.value)}</span>`;
+        } else if (change.type === 'removed') {
+          formattedChange = `<span class="removed">- ${change.path}: ${JSON.stringify(change.value)}</span>`;
+        } else if (change.type === 'modified') {
+          formattedChange = `<span class="modified">~ ${change.path}: ${JSON.stringify(change.oldValue)} -> ${JSON.stringify(change.newValue)}</span>`;
+        }
+        return formattedChange;
+      }).join('\n');
 }
